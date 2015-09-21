8d9af9418d94ac2e8e60e0f028592f6e4cd18bda
basovaelizaveta <basovaelizaveta@gmail.com>
2015-09-21 12:20:48 +0300

    цены

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+	inputWidth();
+})
+
+function inputWidth() {
+	$('.search-field__img input').css({'width':'0px'});
+	$('.search-field__img').on('click', function() {
+		$(this).find('input').animate({
+			'opacity' :'1',
+			'width' : $(this).width()-34 +' px'
+		},'400').focus();
+	});
+
+	$('.search-field__img input').focusout(function() {
+		$(this).animate({
+			'width' : '0px',
+			'opacity' :'0'
+		}), 400;
+	});
+}
